7dc9a6f43c3d871940de4513517a2be64652b86f
KostiantynPolishko <polxs_wp31@student.itstep.org>
2024-06-26 18:17:42 +0300

    2 Created styled-components for inner contents.

--- src/components/CarPhoto/CarPhoto.styled.ts
+++ src/components/CarPhoto/CarPhoto.styled.ts
@@ -8,11 +8,26 @@ const _default: string = 'fit-content';
 interface Size {
     _width?: number,
     _height?: number,
+    _border?: string,
 }
 
 interface Photo {
-    _path?: string,
+    _path?: string,  
 }
+
+interface DisplayContent {
+    _direction?: string;
+    _justify?: string;
+    _align?: string;
+}
+
+const main__br_radius = css`
+    border-radius: 10px 0px 0px 10px;
+`;
+
+const next__br_radius = css`
+    border-radius: 10px 10px 0px 0px;
+`;
 
 export const CarPhotoWrapper = styled.div<Size>`
     position: relative;
@@ -20,9 +35,10 @@ export const CarPhotoWrapper = styled.div<Size>`
     border: 1px solid black;
     width: ${props => props._width? props._width+'px' : _default};
     height: ${props => props._height? props._height+'px' : _default};
+    ${props => props._border === 'main'? main__br_radius : next__br_radius}
 `;
 
-export const PhotoImg = styled.img<Photo>`
+const PhotoImg = styled.img<Photo>`
     background-position: center;
     background-repeat: no-repeat;
     background-size: cover;
@@ -32,5 +48,30 @@ export const CarPhotoImg = styled(PhotoImg).attrs({
     alt: 'cars photo',
 })`
     width: 100%;
+    height: 100%;
+    border-radius: inherit;
+`;
+
+export const Display = styled.div<DisplayContent>`
+    display: flex;
+    flex-direction: ${props => props._direction || 'row'};
+    justify-content: ${props => props._justify || 'space-between' };
+    align-items: ${props => props._align || 'center'};
+`;
+
+export const CarPhotoInner = styled(Display)`
+    position: absolute;
+    top: ${_margin}px;
+    left: ${_margin}px;
+    right: ${_margin}px;
+    bottom: ${_margin}px;
+    padding: ${_padding}px;
+`;
+
+export const ContentTop = styled(Display)`
+    width: 100%;
+`;
+
+export const ContentBottom = styled(Display)`
     height: 100%;
 `;
--- src/components/CarPhoto/CarPhoto.tsx
+++ src/components/CarPhoto/CarPhoto.tsx
@@ -1,12 +1,20 @@
 import React, { FC } from 'react';
-import { CarPhotoWrapper, CarPhotoImg} from './CarPhoto.styled';
+import * as card from './CarPhoto.styled';
 
 interface CarPhotoProps {}
 
 const CarPhoto: FC<CarPhotoProps> = () => (
- <CarPhotoWrapper _width={500} _height={375}>
-   <CarPhotoImg src={require('./img/car_icon.jpg')}/>
- </CarPhotoWrapper>
+ <card.CarPhotoWrapper _width={500} _height={375} _border='main'>
+   <card.CarPhotoImg src={require('./img/car_icon.jpg')}/>
+   <card.CarPhotoInner _direction='column'>
+      <card.ContentTop>
+         
+      </card.ContentTop>
+      <card.ContentBottom>
+
+      </card.ContentBottom>
+   </card.CarPhotoInner>
+ </card.CarPhotoWrapper>
 );
 
 export default CarPhoto;
