cf61dd5aa1e770a24f3dcfd54f00530d1cf513af
KostiantynPolishko <polxs_wp31@student.itstep.org>
2024-06-26 15:19:31 +0300

    Init project baza-gai

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './components/App/App';
+import CarPhoto from './components/CarPhoto/CarPhoto';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,7 +10,8 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App/> */}
+    <CarPhoto/>
   </React.StrictMode>
 );
 
