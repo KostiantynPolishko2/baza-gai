86fcb64d437138b911e1a4662b5690d74b562508
Kostiantyn Polishko <polxs_wp31@student.itstep.org>
2024-06-28 13:29:46 +0300

    1 Created tree for styled-component

---
+++ src/components/FormSearch/FormSearch.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import { FormSearchWrapper } from './FormSearch.styled';
+
+interface FormSearchProps {}
+
+const FormSearch: FC<FormSearchProps> = () => (
+ <FormSearchWrapper>
+    FormSearch Component
+ </FormSearchWrapper>
+);
+
+export default FormSearch;
