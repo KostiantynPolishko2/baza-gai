ab4efdfdcc96c5af07c7f97c51afd14985a588cb
Kostiantyn Polishko <167745085+KostiantynPolishko2@users.noreply.github.com>
2024-06-29 18:41:28 +0300

    release v2.0.0 (#6)
    * Init project baza-gai
    
    * release v1.0.0 (#1) (#4)
    
    * Init project baza-gai
    
    * 1 Created styled-component of main sizes.
    
    * 2 Created styled-components for inner contents.
    
    * 3 Arranged subdivision files of styled-components for FC CarPhoto
    
    General.styled.ts
    CarPhoto.styled.ts
    CardPhotoPlates.styled.ts
    
    * 4 Done styled-components for FlagIcon
    
    * 5 Test commit
    
    * 6 Designed css style for plate number
    
    * Designed styled-component DateRegistr
    
    * 8 Designed styled-component DataMark
    
    * 9 Managed logic for props of CarCardPhoto.
    
    * release v1.1.0 (#3) (#5)
    
    * 1 Created tree for styled-component
    
    * 2 Done css styles for FC FormSearch.
    
    * 3 Done function getDigits for even onClick of button
    
    * 4 Created functions for check format of digits inputted
    
    * 5 Functions has been move  in serateted fiile Functions.tsx
    
    * 6 Make object for useState
    
    * 7 Managed axios request using axios request.
    
    * 8 Designed primery axios request for CarData.
    
    * 9 Were designed array for class CarData() and  class CarError()
    
    * 10 Interfaces and Classes have been serated in file FormIClass.tsx
    
    * 1 Created parent component FormCarPhoto that contain another two child components.
    
    * 2 2 Created tested logic for down pass props from Parent to Child using event handlers with useState
    
    * 3 Managed passing props from Parent FormCarPhoto to child FC FormSearch and FC CarPhoto
    
    * 4 Was done handler of error
    
    * 5 Were managed passing props using instance of class CarData, CarError

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import FormCarPhoto from './components/FormCarPhoto/FormCarPhoto';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,8 +9,10 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+      <body>
+        <FormCarPhoto/>
+      </body>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
