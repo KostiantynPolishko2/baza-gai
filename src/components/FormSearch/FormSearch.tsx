044fe59721be6a25c3634727d585460e74cc5e80
Kostiantyn Polishko <polxs_wp31@student.itstep.org>
2024-06-29 08:29:46 +0300

    8 Designed primery axios request for CarData.

--- src/components/FormSearch/FormSearch.tsx
+++ src/components/FormSearch/FormSearch.tsx
@@ -5,13 +5,43 @@ import './FormSearch.css';
 import * as form from './FormSearch.styled';
 import { getDigits } from './Functions';
 
+interface ICar {
+   digits: string,
+   photo_url: string,
+}
+
+interface ICarData {
+   model: string,
+   model_year: number | string,
+   vendor: string,
+   registered_at: string,
+}
+
+class Car {
+   digits: string;
+   photo_url: string;
+   registered_at: string;
+   model_year: number | string;
+   model: string;
+
+   constructor(car?: ICar, carData?: ICarData){
+      this.digits = car?.digits?? 'XX DDDD XX';
+      this.photo_url = car?.photo_url?? './img/car_icon.jpg';
+
+      this.registered_at = carData?.registered_at?? 'dd.mm.yyyy';
+      this.model_year = carData?.model_year?? 'yyyy';
+      this.model = `${carData?.vendor?? 'vendor'}_${carData?.model?? 'x-model'}`
+   }
+}
+
 interface FormSearchProps {}
 
 const FormSearch: FC<FormSearchProps> = () => {
 
    const [carNumber, setCarNumber] = useState('none');
+   const [carData, setCarData] = useState(new Car());
    
-   const key = '53f98d3aa5e27428971d52008bedee4a';
+   const key = '00cbd51c5f962dfa3b445a42e63d0160';
    const client = axios.create({
        baseURL: 'https://baza-gai.com.ua/nomer/',
        headers: {'Accept': 'application/json', 'X-Api-Key': key},
@@ -22,7 +52,12 @@ const FormSearch: FC<FormSearchProps> = () => {
    useEffect(() => {
       if(carNumber !== 'none'){
          client.get(carNumber).
-         then(response => { console.log(response.data); }).
+         then(response => { 
+            console.log('json', response.data);
+            let car: ICar = response.data;
+            setCarData(new Car(car, response.data['operations']['0']));
+         }).
+
          catch(error => { console.log(error)} )
       }
    }, [carNumber])
@@ -33,8 +68,15 @@ const FormSearch: FC<FormSearchProps> = () => {
            <h3 style={{fontSize: 1.25 + 'rem'}}>Проверка авто по номеру и VIN</h3>
            <form.Input>
               <form.InputSearch  className='form-search'/>
-              <form.InputBtn onClick={e => setCarNumber(getDigits(e))}>Поиск</form.InputBtn>
+              <form.InputBtn onClick={e => setCarNumber(getDigits(e))}>Поиск</form.InputBtn>             
            </form.Input>
+           <p>CarData</p>
+           <ul>
+               <li>{carData.digits}</li>
+               <li>{carData.model}</li>
+               <li>{carData.model_year}</li>
+               <li>{carData.registered_at}</li>
+           </ul>
          </div>
       </form.FormSearchWrapper>
      );
