27c344f6937c0602e91e1751520ce7c03fc8ed10
Kostiantyn Polishko <167745085+KostiantynPolishko2@users.noreply.github.com>
2024-06-29 10:11:41 +0300

    release v1.1.0 (#3) (#5)
    * 1 Created tree for styled-component
    
    * 2 Done css styles for FC FormSearch.
    
    * 3 Done function getDigits for even onClick of button
    
    * 4 Created functions for check format of digits inputted
    
    * 5 Functions has been move  in serateted fiile Functions.tsx
    
    * 6 Make object for useState
    
    * 7 Managed axios request using axios request.
    
    * 8 Designed primery axios request for CarData.
    
    * 9 Were designed array for class CarData() and  class CarError()
    
    * 10 Interfaces and Classes have been serated in file FormIClass.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import FormSearch from './components/FormSearch/FormSearch';
 import CarPhoto from './components/CarPhoto/CarPhoto';
 import reportWebVitals from './reportWebVitals';
 
@@ -9,8 +10,11 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <CarPhoto _url_photo={''} _id={'main'} _digits={'AE 4000 IT'} _registered_at={'28.02.2024'} _model_year={2017} _vendor={'BMW'} _model={'X3'}/>
-  </React.StrictMode>
+      <body>
+        <FormSearch/>
+        <CarPhoto _url_photo={''} _id={'main'} _digits={'AE 4000 IT'} _registered_at={'28.02.2024'} _model_year={2017} _vendor={'BMW'} _model={'X3'}/>
+      </body>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
